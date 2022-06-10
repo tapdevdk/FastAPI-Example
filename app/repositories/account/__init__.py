50993db457418394b9851c6277e85bddc666f8bb
Thor Astrup Pedersen <thorastrup@gmail.com>
2022-06-10 20:53:30 +0200

    Moved accounts get all logic to accounts repository.

--- app/repositories/account/__init__.py
+++ app/repositories/account/__init__.py
@@ -1,23 +1,45 @@
 from app.repositories.account.models import Account
 from app.utils import db_get_client
 
+TABLE_NAME = "accounts"
+KWARG_DB_CONNECTION = "conn"
+
+def get_all(**kwargs):
+    conn = kwargs.get(KWARG_DB_CONNECTION, db_get_client())
+    
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM {TABLE_NAME} ORDER BY created DESC")
+    rows = cursor.fetchall()
+
+    cursor.close()
+    if KWARG_DB_CONNECTION not in kwargs:
+        conn.close()
+
+    return [_map_row_to_account(row) for row in rows]
+
 def get_by_id(id: str, **kwargs):
     return _get_by_field("id", id, **kwargs)
 
 def get_by_username(username: str, **kwargs):
     return _get_by_field("username", username, **kwargs)
 
+# PRIVATE methods
+
 def _get_by_field(field: str, value: str, **kwargs):
     conn = kwargs.get("conn", db_get_client())
     cursor = conn.cursor()
 
-    cursor.execute(f"SELECT * FROM accounts WHERE {field} = %s", (value,))
+    cursor.execute(f"SELECT * FROM {TABLE_NAME} WHERE {field} = %s", (value,))
 
     row = cursor.fetchone()
 
     cursor.close()
-    conn.close()
+    if KWARG_DB_CONNECTION not in kwargs:
+        conn.close()
     
+    return _map_row_to_account(row)
+
+def _map_row_to_account(row):
     return Account(
         id=row['id'],
         username=row['username'],
--- app/routers/accounts.py
+++ app/routers/accounts.py
@@ -1,6 +1,6 @@
 import logging
 from fastapi import APIRouter, HTTPException
-from app.repositories.account import get_by_username
+from app.repositories.account import get_all, get_by_username
 from app.utils import account_get_by_username, db_get_client
 
 logger = logging.getLogger(__name__)
@@ -9,27 +9,11 @@ router = APIRouter()
 
 @router.get("/accounts/", tags=["accounts"])
 async def read_accounts():
-    conn = None
     try:
-        conn = db_get_client()
+        return get_all()
     except Exception as e:
         logger.error(e)
         raise HTTPException(status_code=500, detail="Internal Server Error")
-    
-    try:
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM accounts ORDER BY username ASC")
-        rows = cursor.fetchall()
-
-        return [{
-            "id": row["id"],
-            "username": row["username"],
-            "email": row["email"],
-        } for row in rows]
-    except Exception as e:
-        logger.error(e)
-        raise HTTPException(status_code=500, detail="Internal Server Error")
-
 
 @router.get("/accounts/me", tags=["accounts"])
 async def read_accounts_me():
@@ -39,7 +23,6 @@ async def read_accounts_me():
     except Exception as e:
         logger.error(e)
         raise HTTPException(status_code=500, detail="Internal Server Error")
-
 
 @router.get("/accounts/{username}", tags=["accounts"])
 async def read_user(username: str):
